954fee9aa164dba4fce95f60e05f1eb1ae890df3
Christian Knudsen <chrbknudsen@proton.me>
2026-08-08 16:55:58 +0200

    introspective scale added

--- R/colours.R
+++ R/colours.R
@@ -6,7 +6,7 @@
 psb_palettes <- list(
   Please = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
   Actually = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
-  Introspective = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+  Introspective = c("#eb038b", "#faec03", "#ea1e29", "#02a451", "#eb038b", "#b2208d", "#5d308f" ),
   Behaviour = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
   Very = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
   Bilingual = c("#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
